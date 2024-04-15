8ec9a1396a5f213a40e4e2b2ac5063b17699258c
SiemeM <147379958+SiemeM@users.noreply.github.com>
2024-04-15 16:58:29 +0200

    Login start

---
+++ Login.php
@@ -0,0 +1,44 @@
+<?php
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>IMDFlix</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="netflixLogin">
+		<div class="form form--login">
+			<form action="" method="post">
+				<h2 form__title>Sign In</h2>
+
+				<?php if(isset($error)): ?>
+				<div class="form__error">
+					<p>
+						Sorry, we can't log you in with that email address and password. Can you try again?
+					</p>
+				</div>
+				<?php endif; ?>
+
+				<div class="form__field">
+					<label for="Email">Email</label>
+					<input type="text" name="email">
+				</div>
+				<div class="form__field">
+					<label for="Password">Password</label>
+					<input type="password" name="password">
+				</div>
+
+				<div class="form__field">
+					<input type="submit" value="Sign in" class="btn btn--primary">	
+                    <input type="submit" value="Forgot password" class="btn btn--primary">	
+                    <input type="submit" value="Create an acount" class="btn btn--primary">	
+                    <input type="submit" value="Help" class="btn btn--primary">	
+                    
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
